efcae04f40cbfced3a6dc108fdda025d6f5ffa79
brandon <bwong@konradgroup.com>
2024-08-08 20:50:13 -0400

    update

--- backend/main-backend/src/main/java/com/backend_team/main_backend/controllers/VulnerableController.java
+++ backend/main-backend/src/main/java/com/backend_team/main_backend/controllers/VulnerableController.java
@@ -1,23 +1,23 @@
-package com.backend_team.main_backend.controllers;
+// package com.backend_team.main_backend.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.jdbc.core.JdbcTemplate;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class VulnerableController {
+// @RestController
+// public class VulnerableController {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+//     @Autowired
+//     private JdbcTemplate jdbcTemplate;
 
-    @GetMapping("/user")
-    public String getUser(@RequestParam String username) {
-        // Vulnerable to SQL Injection
-        String sql = "SELECT * FROM users WHERE username = '" + username + "'";
+//     @GetMapping("/user")
+//     public String getUser(@RequestParam String username) {
+//         // Vulnerable to SQL Injection
+//         String sql = "SELECT * FROM users WHERE username = '" + username + "'";
 
-        // Executing the query (this is vulnerable)
-        return jdbcTemplate.queryForObject(sql, String.class);
-    }
-}
+//         // Executing the query (this is vulnerable)
+//         return jdbcTemplate.queryForObject(sql, String.class);
+//     }
+// }
